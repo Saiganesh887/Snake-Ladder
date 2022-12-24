ce75ea4db8813e5a2489290d0b03b0858ae2068f
Your Name <you@example.com>
2022-12-24 13:45:34 +0530

    Player rolls the dice to get a number b/w 1 to 6

--- src/com/bridgelabs/SnakeAndLadder.java
+++ src/com/bridgelabs/SnakeAndLadder.java
@@ -2,8 +2,15 @@ package com.bridgelabs;
 
 public class SnakeAndLadder {
     static final int START_POSITION = 0;
+    static int diceRoll() {
+        int dice = ((int) (Math.random() * 10) % 6) + 1;
+        System.out.println("Dice gives: " + dice);
+        return dice;
+    }
     public static void main(String[] args) {
         System.out.println("Welcome to Snake and Ladder Computation");
+        int diceRoll = diceRoll();
+        System.out.println(diceRoll);
     }
 }
 
